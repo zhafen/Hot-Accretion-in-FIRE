65db14d76ab85a82a4f92989ce894c89e34868e8
Zach Hafen <zachary.h.hafen@gmail.com>
2021-05-01 17:46:02 -0500

    Added a submission script.

---
+++ analysis/submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=hothaloacc
+#SBATCH --partition=skx-normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=48
+#SBATCH --time=12:00:00
+#SBATCH --output=/scratch/03057/zhafen/hot_accretion_data/job_scripts/trove.out
+#SBATCH --error=/scratch/03057/zhafen/hot_accretion_data/job_scripts/trove.err
+#SBATCH --mail-user=zhafen@u.northwestern.edu
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=fail
+#SBATCH --mail-type=end
+
+# Config path
+CONFIG=$1
+
+trove clean $CONFIG
+trove evaluate $CONFIG
+trove execute $CONFIG
