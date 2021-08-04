bfbbdec150a5e144f9c5fd3105ed5a8832ec4f97
Zach Hafen <zachary.h.hafen@gmail.com>
2021-08-04 12:34:01 -0500

    Added jz/jmag figure.

--- analysis/submit.sh
+++ analysis/submit.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=skx-normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=48
-#SBATCH --time=20:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=/scratch/03057/zhafen/hot_accretion_data/logs/trove.out
 #SBATCH --error=/scratch/03057/zhafen/hot_accretion_data/logs/trove.err
 #SBATCH --mail-user=zhafen@u.northwestern.edu
@@ -17,4 +17,4 @@ CONFIG=$1
 
 # trove clean $CONFIG
 # trove evaluate $CONFIG
-trove execute -n 3 $CONFIG
+trove execute -n 4 $CONFIG
