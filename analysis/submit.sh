d60aee310641fb51f130944798abe0e813786ff5
Zach Hafen <zachary.h.hafen@gmail.com>
2021-11-29 13:08:41 -0600

    Major figure update.

--- analysis/submit.sh
+++ analysis/submit.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=skx-normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=48
-#SBATCH --time=12:00:00
+#SBATCH --time=8:00:00
 #SBATCH --output=/scratch/03057/zhafen/hot_accretion_data/logs/trove.out
 #SBATCH --error=/scratch/03057/zhafen/hot_accretion_data/logs/trove.err
 #SBATCH --mail-user=zhafen@u.northwestern.edu
